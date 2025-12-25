eb4b9a1f92c178a1be3ae163973c4efdc6eea2d2
bala-amypo <bala@amypo.in>
2025-12-25 08:26:33 +0000

    User commit

--- src/main/java/com/example/demo/service/AnomalyRuleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.AnomalyRule;
-import java.util.List;
-
-public interface AnomalyRuleService {
-    AnomalyRule createRule(AnomalyRule rule);
-    List<AnomalyRule> getAllRules();
-}
