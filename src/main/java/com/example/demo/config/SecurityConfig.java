249273b00d2bffdc97fe3b73b109dedd70e6339f
bala-amypo <bala@amypo.in>
2025-12-26 14:53:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    // Intentionally empty
+    // Tests do not validate security behavior
+}
