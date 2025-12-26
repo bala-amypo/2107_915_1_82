3609c0b73b0d12e05b77e96c130408f3e6255142
bala-amypo <bala@amypo.in>
2025-12-26 12:36:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/TeamSummaryRecordRepository.java
+++ src/main/java/com/example/demo/repository/TeamSummaryRecordRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.TeamSummaryRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.TeamSummaryRecord;
 
 public interface TeamSummaryRecordRepository
         extends JpaRepository<TeamSummaryRecord, Long> {
