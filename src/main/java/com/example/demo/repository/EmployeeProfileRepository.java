f9c529a34dcb6df951b4f9735cf90f189e63dfd8
bala-amypo <bala@amypo.in>
2025-12-28 08:49:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeProfileRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeProfileRepository.java
@@ -3,10 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.EmployeeProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface EmployeeProfileRepository
         extends JpaRepository<EmployeeProfile, Long> {
 
-    Optional<EmployeeProfile> findByEmployeeId(String employeeId);
+    EmployeeProfile findByEmployeeId(String employeeId);
 }
--- src/main/java/com/example/demo/repository/ProductivityMetricRecordRepository.java
+++ src/main/java/com/example/demo/repository/ProductivityMetricRecordRepository.java
@@ -4,12 +4,11 @@ import com.example.demo.model.ProductivityMetricRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductivityMetricRecordRepository
         extends JpaRepository<ProductivityMetricRecord, Long> {
 
-    Optional<ProductivityMetricRecord> findByEmployeeId(Long employeeId);
+    ProductivityMetricRecord findByEmployeeId(Long employeeId);
 
     List<ProductivityMetricRecord> findAllByEmployeeId(Long employeeId);
 }
