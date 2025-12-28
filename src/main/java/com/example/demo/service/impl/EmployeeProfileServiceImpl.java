4ea9f6bd0c85a27bdcab72588485941f5f4fd8cd
bala-amypo <bala@amypo.in>
2025-12-28 18:08:38 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -54,8 +54,6 @@ public class EmployeeProfileServiceImpl implements EmployeeProfileService {
 
     @Override
     public Optional<EmployeeProfile> findByEmployeeId(String employeeId) {
-        return Optional.ofNullable(
-                repository.findByEmployeeId(employeeId)
-        );
+        return repository.findByEmployeeId(employeeId);
     }
 }
